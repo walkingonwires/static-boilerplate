b2ba5f550604a21bf8d179d2f3f640c41814827d
andrew <andrew@andrews-MacBook-Pro-2.local>
2016-06-03 10:41:42 -0400

    removed stuff

--- src/js/lib/app.js
+++
@@ -1,9 +0,0 @@
-page('*', init.ctx);
-page('/', route.home, render.content);
-page('/work', route.work, render.content);
-page('/what-we-do', route.whatwedo, render.content);
-page('/about-us', route.aboutus, render.content);
-page('/jobs', route.jobs, render.content);
-page('/labs', route.labs, render.content);
-page('*', route.error, render.content);
-page();
