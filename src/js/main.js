dd151866e0fb72a46815a2a2bde97c99a48f9203
Adam Grayson <applead@aim.com>
2016-06-10 14:08:47 -0400

    registered a partial without any errors

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,6 @@ var hbs = require('handlebars/runtime').default,
 
 window.page = page;
 router();
+
+var labTile = require('../templates/partials/lab-tile.hbs');
+hbs.registerPartial('labTile', labTile());
