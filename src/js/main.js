595eba35256a986c591fe165dbe8f5ffc307aca5
Adam Grayson <applead@aim.com>
2017-01-04 16:19:14 -0500

    partial example

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,9 @@ var hbs = require('handlebars/runtime').default,
 	page = require('page'),
 	router = require('./router/router');
 
+hbs.registerPartial({
+    'example': require('../templates/partials/example.hbs')
+});
+
 window.page = page;
 router();
